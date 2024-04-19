8d61aa382a72f49d06c8c842f67d10d8addea917
Ajeet <167222316+Ajeet-aj@users.noreply.github.com>
2024-04-19 17:20:51 +0530

    Add files via upload

---
+++ Project-6 - Bulk Rename/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const replaceThis = "ravi"
+const replaceWith = "john"
+const preview = true
+const folder = __dirname
+
+try {
+  fs.readdir(folder, (err, data) => {
+
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      let oldFile = path.join(folder, item)
+      let newFile = path.join(folder, item.replaceAll(replaceThis, replaceWith))
+      if (preview) {
+        fs.rename(oldFile, newFile, () => {
+          console.log("Rename Success", item, newFile)
+        })
+
+      } else {
+        if ("data/" + item !== newFile) console.log("" + item + "will be rename to" + newFile)
+      }
+    }
+  })
+} catch (err) {
+  console.error(err);
+}
